bf172c77b930bc929ff5dc151ab02be24dbbdb98
Piotr Obrochta <piotr.obrochta88@gmail.com>
2023-03-22 11:11:58 +0100

    I start writing js.script

---
+++ script.js
@@ -0,0 +1,19 @@
+{
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
+
+    const toggleTaskDone = (taskIndex) => {
+        task[taskIndex].done = !tasks[tasksIndex].done;
+        redner();
+    }
+}
